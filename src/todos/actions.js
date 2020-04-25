9e15d443de89eba30fab1da59e20c4a7b38b0697
jpibarra1130 <jpibarra1130@gmail.com>
2020-04-25 09:03:54 +0800

    Actions for the application so far

---
+++ src/todos/actions.js
@@ -0,0 +1,38 @@
+export const CREATE_TODO = 'CREATE_TODO';
+
+// we need to have an argument `text` here to receive the payload
+// once this action is triggered
+// this is called `action creators`
+// best practice is to use action creators instead of bare actions
+export const createTodo = text => ({
+    type: CREATE_TODO,
+    payload: { text },
+});
+
+export const REMOVE_TODO = 'REMOVE_TODO';
+export const removeTodo = text => ({
+    type: REMOVE_TODO,
+    payload: { text },
+});
+
+export const COMPLETE_TODO = 'COMPLETE_TODO';
+export const completeTodo = text => ({
+    type: COMPLETE_TODO,
+    payload: { text },
+});
+
+export const LOAD_TODOS_IN_PROGRESS = 'LOAD_TODOS_IN_PROGRESS';
+export const loadTodosInProgress = () => ({
+    type: LOAD_TODOS_IN_PROGRESS,
+});
+
+export const LOAD_TODOS_SUCCESS = 'LOAD_TODOS_SUCCESS';
+export const loadTodosSuccess = todos => ({
+    type: LOAD_TODOS_SUCCESS,
+    payload: { todos },
+});
+
+export const LOAD_TODOS_FAILURE = 'LOAD_TODOS_FAILURE';
+export const loadsTodosFailure = () => ({
+    type: LOAD_TODOS_FAILURE,
+});
